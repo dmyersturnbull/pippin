db943656f16f725e2baccb5ce1aeb45c37f740bc
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2016-11-17 18:25:29 -0800

    Finally fixed issue #10! Tests run and pass.

--- misc/src/test/scala/kokellab/utils/misc/BarcoderTest.scala
+++ misc/src/test/scala/kokellab/utils/misc/BarcoderTest.scala
@@ -3,73 +3,51 @@ package kokellab.utils.misc
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 
 import com.google.zxing.BarcodeFormat
-
 import org.scalacheck.Gen
-import org.scalatest._
-import prop._
-
+import org.scalacheck.Prop.BooleanOperators
+import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
+import org.scalatest.{Matchers, PropSpec}
 
 class BarcoderTest extends PropSpec with GeneratorDrivenPropertyChecks with Matchers {
 
-	val imageFormatGen = Gen.oneOf("png", "jpg", "gif")
+		def genBoundedList[T](maxSize: Int, gen: Gen[T]): Gen[List[T]] =
+			Gen.choose(0, maxSize) flatMap (size => Gen.listOfN(size, gen))
 
-	val rectangularGen: Gen[(Int, Int)] = for {
-		width <- Gen.choose(20, 100)
-		height <- Gen.choose(20, 100)
-	} yield (width, height)
+		def genBoundedString(maxSize: Int, gen: Gen[Char]): Gen[String] =
+			Gen.choose(0, maxSize) flatMap (size => Gen.listOfN(size, gen) map (_.mkString))
 
-	val squareGen: Gen[(Int, Int)] = for {
-		size <- Gen.choose(20, 100)
-	} yield (size, size)
+		def encodeDecode(text: String, codeFormat: BarcodeFormat, dimensions: (Int, Int), imageFormat: String): String = {
+			val barcoder = new Barcoder(codeFormat, imageFormat, dimensions._1, dimensions._2)
+			val os = new ByteArrayOutputStream()
+			barcoder.encode(text, os)
+			val is = new ByteArrayInputStream(os.toByteArray)
+			barcoder.decode(is)
+		}
 
-	val code39And93Gen: Gen[String] = Utils.genBoundedString(48, Gen.frequency((36, Gen.alphaNumChar), (7, Gen.oneOf('-', '.', '$', '/', '+', '%', ' '))))
-
-//		def test(barcodeFormat: BarcodeFormat, dimensionsGen: Gen[(Int, Int)], stringGen: Gen[String]) = {
-//			property(s"Decoding an encoded string should yield the original string for ${barcodeFormat.name} codes") {
-//				forAll(imageFormatGen, stringGen, dimensionsGen) { (imageFormat: String, text: String, dimensions: (Int, Int)) =>
-//					println("SDFDSF")
-//					whenever (!text.trim.isEmpty) {
-//						encodeDecode(text.toUpperCase, barcodeFormat, dimensions, imageFormat) should equal(text.toUpperCase)
-//					}
-//				}
-//			}
-//		}
-
-		property(s"Decoding an encoded string should yield the original string for ${BarcodeFormat.CODE_39.name} codes") {
-			forAll(imageFormatGen, Utils.genBoundedString(48, Gen.choose[Char](0, 127)), rectangularGen) { (imageFormat: String, text: String, dimensions: (Int, Int)) =>
-				println("SDFDSF")
-				whenever (!text.trim.isEmpty) {
-					Utils.encodeDecode(text.toUpperCase, BarcodeFormat.CODE_39, dimensions, imageFormat) should equal(text.toUpperCase)
+		val imageFormatGen = Gen.oneOf("png", "jpg", "gif")
+		def test(barcodeFormat: BarcodeFormat, dimensionsGen: Gen[(Int, Int)], stringGen: Gen[String]) = {
+			property(s"Decoding an encoded string should yield the original string for ${barcodeFormat.name} codes") {
+				forAll(imageFormatGen, stringGen, dimensionsGen) { (imageFormat: String, text: String, dimensions: (Int, Int)) =>
+					(!text.trim.isEmpty) ==> (encodeDecode(text.toUpperCase, barcodeFormat, dimensions, imageFormat) == text.toUpperCase)
 				}
 			}
-	}
-//		test(BarcodeFormat.CODE_39, rectangularGen, code39And93Gen)
-//		test(BarcodeFormat.CODE_93, rectangularGen, code39And93Gen)
-//
-//		test(BarcodeFormat.CODE_128, rectangularGen, genBoundedString(48, Gen.choose[Char](0, 127)))
-//
-//		test(BarcodeFormat.QR_CODE, squareGen, genBoundedString(4296, Gen.frequency((36, Gen.alphaNumChar), (8, Gen.oneOf('-', '.', '$', '/', '+', '%', ' ', ':')))))
+		}
 
-//	val fullGen: Gen[(BarcodeFormat, Gen[(Int, Int)], Gen[String])] = Gen.oneOf(Seq(
-//		(BarcodeFormat.CODE_39, rectangularGen, code39And93Gen)
-//	))
+		val rectangularGen: Gen[(Int, Int)] = for {
+			width <- Gen.choose(20, 100)
+			height <- Gen.choose(20, 100)
+		} yield (width, height)
 
-}
+		val squareGen: Gen[(Int, Int)] = for {
+			size <- Gen.choose(20, 100)
+		} yield (size, size)
 
-object Utils {
+		val code39And93Gen: Gen[String] = genBoundedString(48, Gen.frequency((36, Gen.alphaNumChar), (7, Gen.oneOf('-', '.', '$', '/', '+', '%', ' '))))
+		test(BarcodeFormat.CODE_39, rectangularGen, code39And93Gen)
+		test(BarcodeFormat.CODE_93, rectangularGen, code39And93Gen)
 
-	def genBoundedList[T](maxSize: Int, gen: Gen[T]): Gen[List[T]] =
-		Gen.choose(0, maxSize) flatMap (size => Gen.listOfN(size, gen))
+		test(BarcodeFormat.CODE_128, rectangularGen, genBoundedString(48, Gen.choose[Char](0, 127)))
 
-	def genBoundedString(maxSize: Int, gen: Gen[Char]): Gen[String] =
-		Gen.choose(0, maxSize) flatMap (size => Gen.listOfN(size, gen) map (_.mkString))
-
-	def encodeDecode(text: String, codeFormat: BarcodeFormat, dimensions: (Int, Int), imageFormat: String): String = {
-		val barcoder = new Barcoder(codeFormat, imageFormat, dimensions._1, dimensions._2)
-		val os = new ByteArrayOutputStream()
-		barcoder.encode(text, os)
-		val is = new ByteArrayInputStream(os.toByteArray)
-		barcoder.decode(is)
-	}
+		test(BarcodeFormat.QR_CODE, squareGen, genBoundedString(4296, Gen.frequency((36, Gen.alphaNumChar), (8, Gen.oneOf('-', '.', '$', '/', '+', '%', ' ', ':')))))
 
 }
--- webservices/src/test/scala/kokellab/utils/webservices/ChemspiderTest.scala
+++ webservices/src/test/scala/kokellab/utils/webservices/ChemspiderTest.scala
@@ -1,33 +1,29 @@
 package kokellab.utils.webservices
 
-import org.scalacheck.Gen
-import org.scalatest.prop.PropertyChecks
-import org.scalatest.{Matchers, PropSpec}
+import org.scalatest._
+import prop._
 
-class ChemspiderTest extends PropSpec with PropertyChecks with Matchers {
 
-	def test() = {
-		val spider = new Chemspider
-		val smilesGen = Gen.oneOf("C[C@@H](c1ccc(cc1)CC(C)C)C(=O)O", "CC(=O)Oc1ccccc1C(=O)O")
-		property(s"Should get the same smiles string back") {
-			forAll(smilesGen) { (smiles: String) => {
-				val ids = spider.fetchChemspiderIds(smiles)
-				ids foreach (id => {
-					val fetched = spider.fetchBasicInfo(id)
-					smiles should equal (fetched.smiles)
-				})
-				}
-			}
+class ChemspiderTest extends PropSpec with TableDrivenPropertyChecks with Matchers {
+
+	val spider = new Chemspider
+
+	val realSmiles = Table("C[C@@H](c1ccc(cc1)CC(C)C)C(=O)O", "CC(=O)Oc1ccccc1C(=O)O")
+	property(s"Should get the same smiles string back") {
+		forAll(realSmiles) { (smiles: String) => {
+			val ids = spider.fetchChemspiderIds(smiles)
+			ids foreach (id => {
+				val fetched = spider.fetchBasicInfo(id)
+				smiles should equal (fetched.smiles)
+			})
+		}
 		}
 	}
 
-	def nullTest() = {
-		val spider = new Chemspider
-		val smilesGen = Gen.oneOf("C[C@@H]sadgasdfgasgsag", "CC(=O)asdgasdgasdg")
-		property(s"Should return an empty sequence") {
-			forAll(smilesGen) { (smiles: String) =>
-				spider.fetchChemspiderIds(smiles) should equal(null)
-			}
+	val badSmiles = Table("[CC[12354235", "==CCSDF")
+	property(s"Should return an empty sequence") {
+		forAll(badSmiles) { (smiles: String) =>
+			spider.fetchChemspiderIds(smiles) should be (empty)
 		}
 	}
 
