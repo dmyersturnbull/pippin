14cc6994f1a60e3933b7898cdfa16c885d94c475
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2016-11-27 12:23:04 -0800

    Fixed issue #14 and bugs in RgbColor.

--- core/src/main/scala/kokellab/utils/core/addons/RgbColor.scala
+++ core/src/main/scala/kokellab/utils/core/addons/RgbColor.scala
@@ -4,18 +4,24 @@ package kokellab.utils.core.addons
 	* A representation of an RGB color convertible between a 6-digit hex representation and unsigned short values for red, green, and blue.
 	*/
 case class RgbColor(red: Short, green: Short, blue: Short) {
-	def toHex: String = Integer.toHexString(red) + Integer.toHexString(green) + Integer.toHexString(blue)
+	def toHex: String = (Seq(red, green, blue) map (c => Integer.toHexString(c).reverse.padTo(2, '0').reverse)).mkString
 }
 
 object RgbColor {
+
 	def fromHex(string: String): RgbColor = {
-		val c = (Range.inclusive(0, 4, 2) map (i => Integer.parseInt(string.toUpperCase.substring(i, i + 2), 16).toShort)).toList
+		val c = (Range.inclusive(0, 4, 2) map (i => {
+			Integer.parseInt(string.toUpperCase.substring(i, i + 2), 16).toShort
+		})).toList
 		RgbColor(c(0), c(1), c(2))
 	}
+
 	def blend(colors: Seq[RgbColor]) = {
-		val sum = colors.foldLeft((0, 0, 0))((blended, next) =>
-			(blended._1 + next.red, blended._2 + next.green, blended._3 + next.blue) // casts to Int
+		if (colors.isEmpty) throw new IllegalArgumentException("Could not blend from zero colors")
+		val sum = colors.foldLeft(Seq(0, 0, 0))((blended, next) =>
+			Seq(blended(0) + next.red, blended(1) + next.green, blended(2) + next.blue) // casts to Int
 		)
-		RgbColor((sum._1 / colors.size).toShort, (sum._2 / colors.size).toShort, (sum._3 / colors.size).toShort)
+		val z = sum map (c => Math.round(c / colors.size.toFloat).toShort)
+		RgbColor(z(0), z(1), z(2))
 	}
 }
--- core/src/test/scala/kokellab/utils/core/addons/RgbColorTest.scala
+++ core/src/test/scala/kokellab/utils/core/addons/RgbColorTest.scala
@@ -1,26 +1,31 @@
 package kokellab.utils.core.addons
-import java.util.regex.Pattern
 
 import org.scalacheck.Gen
-import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
+import org.scalatest.prop.{GeneratorDrivenPropertyChecks, TableDrivenPropertyChecks}
 import org.scalatest.{Matchers, PropSpec}
 
 
-class RgbColorTest extends PropSpec with GeneratorDrivenPropertyChecks with Matchers {
+class RgbColorTest extends PropSpec with GeneratorDrivenPropertyChecks with TableDrivenPropertyChecks with Matchers {
 
-
-	val gen = Gen.listOfN(6, Gen.alphaNumChar) map (_.mkString)
-
-//	val pattern = Pattern.compile("[0-9A-Za-z]{6}")
+	val gen = Gen.listOfN(6, Gen.frequency((10, Gen.numChar), (6, Gen.oneOf('A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f')))) map (_.mkString)
+	val things = Table("0000ff", "0000ff")
 
 	property(s"toHex(fromHex)) should be the identity") {
-		forAll(gen) { (hexColor: String) => {
-			// TODO I have NO idea why this sometimes isn't 6
-			// https://github.com/rickynils/scalacheck/issues/304
-			println(hexColor.length)
-			(RgbColor.fromHex(hexColor).toHex) should equal (hexColor)
-		}
-	}}
+		forAll(gen) { (hexColor: String) =>
+			RgbColor.fromHex(hexColor).toHex should equal (hexColor.toLowerCase)
+		}}
+
+	val blendGen: Gen[(List[String], String)] = Gen.oneOf(
+		(List("CC3366", "99FF00"), "B39933"),
+		(List("ff0000", "0000ff", "ff00ff"), "aa00aa")
+	)
+
+	property(s"Colors should blend correctly") {
+		forAll(blendGen) { case (colors: Seq[String], correct: String) =>
+			if (colors.nonEmpty) {
+				RgbColor.blend(colors map RgbColor.fromHex).toHex should equal (correct.toLowerCase)
+			}
+		}}
 
 
 }
