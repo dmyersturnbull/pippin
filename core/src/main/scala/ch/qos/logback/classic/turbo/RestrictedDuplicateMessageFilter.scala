71da27a23aaa5dbc1c22e161a9b4ed286848604c
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2017-01-19 09:11:49 -0800

    Better fix for issue #22.

--- core/src/main/scala/ch/qos/logback/classic/turbo/RestrictedDuplicateMessageFilter.scala
+++ core/src/main/scala/ch/qos/logback/classic/turbo/RestrictedDuplicateMessageFilter.scala
@@ -13,8 +13,9 @@ import scala.beans.BeanProperty
   * @param packagesAlwaysAllowed Comma-separated list of fully qualified package names: Permit any number of messages with the same format if they're from any package descended from any of these packages
   * @param nRepetitionsAllowed Permit at most this number of messages with the same format
   * @param cacheSize Store up to this many message formats for comparison; if more message formats are seen than this number, new messages with formats seen previously may be logged despite exceeding the count limit
+  * @param fixedLogLevel Use this SLF4J log level to state that further messages will be ignored; if < 0, the same level as the log message ignored will be used
   */
-class RestrictedDuplicateMessageFilter(@BeanProperty var alwaysDebug: Bool, @BeanProperty var packagesAlwaysAllowed: String, @BeanProperty var nRepetitionsAllowed: Int = 1, @BeanProperty var cacheSize: Int = 200) extends TurboFilter {
+class RestrictedDuplicateMessageFilter(@BeanProperty var packagesAlwaysAllowed: String, @BeanProperty var nRepetitionsAllowed: Int = 1, @BeanProperty var cacheSize: Int = 200, @BeanProperty var fixedLogLevel: Bool=-1) extends TurboFilter {
 
 	private var msgCache: LRUMessageCache = null
 
@@ -25,7 +26,8 @@ class RestrictedDuplicateMessageFilter(@BeanProperty var alwaysDebug: Bool, @Bea
 		else msgCache.getMessageCountAndThenIncrement(format) match {
 			case x if x < nRepetitionsAllowed => FilterReply.NEUTRAL
 			case x if x == nRepetitionsAllowed =>
-				logger.log(marker, Logger.FQCN, slf4jLevel(level), s"...ignoring any further messages with the format $format.", null, throwable)
+				val slf4jl = if (fixedLogLevel < 0) slf4jLevel(level) else fixedLogLevel
+				logger.log(marker, Logger.FQCN, slf4jl, s"...ignoring any further messages with the format $format.", null, throwable)
 				FilterReply.DENY
 			case _ => FilterReply.DENY
 		}
@@ -45,7 +47,7 @@ class RestrictedDuplicateMessageFilter(@BeanProperty var alwaysDebug: Bool, @Bea
 	/**
 	  * Defined in org.slf4j.spi.LocationAwareLogger.
 	  */
-	private def slf4jLevel(level: Level): Int = if (alwaysDebug) 10 else Map(
+	private def slf4jLevel(level: Level): Int = Map(
 		Level.TRACE -> 0,
 		Level.DEBUG -> 10,
 		Level.INFO -> 20,
