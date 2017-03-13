4c30c5162720bda7a3158553573532c7c41a5c9e
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2017-03-12 19:42:05 -0700

    Use val so we can access it.

--- grammars/src/main/scala/kokellab/utils/grammars/GrammarException.scala
+++ grammars/src/main/scala/kokellab/utils/grammars/GrammarException.scala
@@ -2,4 +2,6 @@ package kokellab.utils.grammars
 
 import org.parboiled2.ParseError
 
-class GrammarException(message: String, verboseMessage: Option[String] = None, underlying: Option[ParseError] = None) extends Exception(message + "\n" + verboseMessage.getOrElse(""), underlying.orNull)
+class GrammarException(val message: String, val verboseMessage: Option[String] = None, val underlying: Option[ParseError] = None) extends Exception(message + "\n" + verboseMessage.getOrElse(""), underlying.orNull) {
+
+}
