adba323ceb9108372a000f1e253913189183f55a
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2017-03-25 21:06:20 -0700

    Made TimeSeriesGrammar stream if possible (issue #33) and also added out-of-bounds handling with NaNs, amazingly fixing issue #31.

--- grammars/src/main/scala/kokellab/utils/grammars/TimeSeriesGrammar.scala
+++ grammars/src/main/scala/kokellab/utils/grammars/TimeSeriesGrammar.scala
@@ -10,10 +10,10 @@ import scala.util.matching.Regex.Match
 object TimeSeriesGrammar {
 
 	def build[A : ClassTag](expression: String, start: Int, stop: Int, converter: Double => A,
-			  randBasis: Option[RandBasis] = None, defaultValue: Byte = 0, tolerance: Double = BooleanRealNumberGrammar.DEFAULT_TOLERANCE
-			 ): Array[A] = {
+			  randBasis: Option[RandBasis] = None, defaultValue: Byte = 0, outOfBoundsValue: Double = Double.NaN, tolerance: Double = BooleanRealNumberGrammar.DEFAULT_TOLERANCE
+			 ): TraversableOnce[A] = {
 		val (value, interval) = extract(expression)
-		new TimeSeriesGrammar(value, start, stop, converter, randBasis, interval, defaultValue, tolerance).build()
+		new TimeSeriesGrammar(value, start, stop, converter, randBasis, interval, defaultValue, outOfBoundsValue = outOfBoundsValue, tolerance = tolerance).build()
 	}
 
 	private val mainPattern = """ *(.*?) *(?: +(?:evaluate every)|@ +(\d+))?""".r
@@ -30,60 +30,88 @@ object TimeSeriesGrammar {
 }
 
 class TimeSeriesGrammar[@specialized(Float, Double, Byte, Short, Int) A : ClassTag](expression: String, start: Int, stop: Int, converter: Double => A,
-						randBasis: Option[RandBasis] = None, evaluationInterval: Int = 1, defaultValue: Double = 0, tolerance: Double = BooleanRealNumberGrammar.DEFAULT_TOLERANCE) {
+						randBasis: Option[RandBasis] = None, evaluationInterval: Int = 1, defaultValue: Double = 0, outOfBoundsValue: Double = Double.NaN, tolerance: Double = BooleanRealNumberGrammar.DEFAULT_TOLERANCE) {
 
 	private val tPattern = """\$t(?![\[A-Za-z0-9_])""".r
 	private val tArrayPattern = """\$t\[([^\]]+)\]""".r
 
-	def build(): Array[A] = {
+	private def arrayAccessReplacer(values: Array[A])(m: Match): String = {
+		// we could accept even more sophisticated indexing, such as if-else or real numbers
+		// but this is safer and more efficient
+		val index = IntegerGrammar.eval(m.group(1))
+		val result = if (index >= start && index < values.length * evaluationInterval) values(index / evaluationInterval).toString
+		else outOfBoundsValue.toString
+		result
+	}
+
+	private def fullReplacer(values: Array[A]) = {
+		val r = arrayAccessReplacer(values) _
+		(i: Int) => {
+			// substitute $t and then $t[index]
+			// the order is essential so that the array index can reference $t
+			val tReplaced = tPattern.replaceAllIn(expression, i.toString)
+			tArrayPattern.replaceAllIn(tReplaced, r)
+		}
+	}
+
+	private val simpleReplacer = (i: Int) => tPattern.replaceAllIn(expression, i.toString)
+
+	def build(): TraversableOnce[A] = {
 
 		if (Try(converter(expression.toDouble)).isSuccess) {
-			return Array.fill[A](stop - start)(converter(expression.toDouble))
-		}
+			Iterator.fill[A](stop - start)(converter(expression.toDouble))
+		} else if (expression contains '[') {
+			buildWithArrayAccess()
+		} else if (expression contains "$") {
+			buildStreaming()
+		} else throw new AssertionError()
+
+	}
+
+	private def buildWithArrayAccess(): Array[A] = {
 
 		// we're going to build this up and access it with $t[index] as needed
-		var values: Array[A] = Array.ofDim[A](stop - start)
+		val values: Array[A] = Array.ofDim[A]((stop - start) / evaluationInterval)
 
-		// replaces any single $t[index] with index evaluated by the RealNumberGrammar, whose result is truncated to Int
-
-		def arrayAccessReplacer = (m: Match) => {
-			// NOTE: truncates to Int!
-			val index = RealNumberGrammar.eval(m.group(1), randBasis).toInt
-			if (index > -1 && index < values.length) values(index).toString
-			else defaultValue.toString
-		}
-
-		val substitute = if (expression contains '[') {
-			(string: String, i: Int) => {
-				// substitute $t and then $t[index]
-				// the order is essential so that the array index can reference $t
-				val tReplaced = tPattern.replaceAllIn(string, i.toString)
-				tArrayPattern.replaceAllIn(tReplaced, arrayAccessReplacer)
-			}
-		} else if (expression contains '$') {
-			(string: String, i: Int) => tPattern.replaceAllIn(string, i.toString)
-		} else {
-			(string: String, i: Int) => string
-		}
+		val replacer = fullReplacer(values)
 
 		// calculates the if-elif-else expression for an index, replacing $t and $t[index] as needed
 		val calculate: Int => Double = if (expression containsSlice "if") {
-			i: Int => IfElseRealNumberGrammar.eval(substitute(expression, i), tolerance = tolerance, randBasis = randBasis).getOrElse(defaultValue)
+			i: Int => IfElseRealNumberGrammar.eval(replacer(i), tolerance = tolerance, randBasis = randBasis).getOrElse(defaultValue)
 		} else {
-			i: Int => RealNumberGrammar.eval(substitute(expression, i), randBasis = randBasis)
+			i: Int => RealNumberGrammar.eval(replacer(i), randBasis = randBasis)
 		}
 
 		var lastValue: A = converter(0.0)
-		for (i <- start until stop) {
-			values(i) = {
-				if (i % evaluationInterval == 0 || i == start) converter{
-					lastValue = values.last
-					calculate(i)
-				} else lastValue
-			}
+		for (i <- start until stop) yield {
+			lastValue = if (i % evaluationInterval == 0 || i == start) converter(calculate(i))
+			else lastValue
+			values(i / evaluationInterval) = lastValue
+			lastValue
 		}
 
 		values
 	}
+
+	private def buildStreaming(): TraversableOnce[A] = {
+
+		val replacer = simpleReplacer
+
+		// send to IfElse grammar liberally to make errors easier; ex: send "5 else 2" to IfElse
+		val calculate: Int => Double = if ((expression containsSlice "if") || (expression containsSlice "else") || (expression containsSlice "elif")) {
+			i: Int => IfElseRealNumberGrammar.eval(replacer(i), tolerance = tolerance, randBasis = randBasis).getOrElse(defaultValue)
+		} else {
+			i: Int => RealNumberGrammar.eval(replacer(i), randBasis = randBasis)
+		}
+
+		var lastValue: A = converter(0.0)
+		for (i <- start until stop) yield {
+			lastValue = if (i % evaluationInterval == 0 || i == start) converter(calculate(i))
+			else lastValue
+			lastValue
+		}
+
+	}
+
 
 }
