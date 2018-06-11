8f8067fa0f07077d86d118bf3c377b3630119a9b
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2018-06-11 15:45:38 -0700

    Deleted the deprecated SevenZip, which needed apache commons compress.

--- misc/build.sbt
+++ misc/build.sbt
@@ -1,6 +1,5 @@
 name := "skale-misc"
 libraryDependencies ++= Seq(
-	"org.apache.commons" % "commons-compress" % "1.13",
 	"org.tukaani" % "xz" % "1.8",
 	"com.google.zxing" % "core" % "3.3.2",
 	"com.google.zxing" % "javase" % "3.3.2",
--- misc/src/main/scala/kokellab/utils/misc/SevenZip.scala
+++ misc/src/main/scala/kokellab/utils/misc/SevenZip.scala
@@ -6,7 +6,6 @@ import java.nio.file.{Path, Paths}
 import sys.process._
 import com.typesafe.scalalogging.LazyLogging
 import kokellab.utils.core.exceptions.ServiceFailedException
-import org.apache.commons.compress.archivers.sevenz.{SevenZArchiveEntry, SevenZFile}
 
 import scala.util.{Failure, Success, Try}
 
@@ -24,40 +23,3 @@ object SevenZipCommandLine extends LazyLogging {
 }
 
 
-@deprecated("This code is based on apache commons-compress, which has a lot of problems that make this unreliable. See [[SevenZip.un7zip]] instead, which requires 7z available externally..", "0.1.4")
-object SevenZip extends LazyLogging {
-
-	def un7zip(path: Path, writePath: Path): Unit = {
-
-		writePath.toFile.mkdir()
-		val seven = new SevenZFile(path.toFile)
-
-		try {
-			var entry: SevenZArchiveEntry = null
-			do {
-				entry = seven.getNextEntry
-				if (entry != null) writeEntry(seven, entry, writePath)
-			} while (entry != null)
-		} finally seven.close()
-	}
-
-	private def writeEntry(seven: SevenZFile, entry: SevenZArchiveEntry, writePath: Path) = {
-
-		val outFile = Paths.get(writePath.toFile.getPath, entry.getName)
-		require(!outFile.toFile.exists, s"File $outFile already exists; refusing to overwrite")
-
-		val os = new FileOutputStream(outFile.toFile, true)
-
-		var status: Int = 1
-		try {
-			while (status != -1) {
-				val buffer = new Array[Byte](entry.getSize.toInt)
-				status = seven.read(buffer)
-				if (status != -1) os.write(buffer)
-			}
-		} finally os.close()
-
-		logger.debug(s"Extracted ${entry.getName} to $outFile")
-	}
-
-}
